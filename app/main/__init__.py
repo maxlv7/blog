ffd25098b90bf95a95ecc4d9a114291da6aab51e
maxlv7 <admin@maxlv.org>
2018-03-07 19:50:36 +0800

    add auth blueprint

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 
 """创建蓝本"""
 
-main = Blueprint('main',__name__,static_folder='../assets',template_folder='./templates')
+main = Blueprint('main',__name__,static_folder='./assets',template_folder='./templates')
 
 from . import views
 
