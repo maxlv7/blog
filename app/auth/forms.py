83fb1760f7b5326d3eb3bbc7f786504646bd1d11
maxlv7 <admin@maxlv.org>
2018-03-08 21:37:39 +0800

    add add forms

---
+++ app/auth/forms.py
@@ -0,0 +1,22 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,SubmitField
+from wtforms import ValidationError
+from wtforms.validators import Required,Length,Email,EqualTo
+from ..models import User
+
+# 登陆表单
+class LoginForm(FlaskForm):
+    email = StringField("邮箱",validators=[Required(),Length(1,64),Email()])
+    password = PasswordField("密码",validators=[Required()])
+    submit_log = SubmitField("登录")
+
+# 注册表单
+class RegistrationForm(FlaskForm):
+    email = StringField('电子邮箱', validators=[Required(), Length(1, 64),
+                                                Email()])
+    username = StringField('你的昵称', validators=[Required(), Length(1, 64)])
+    password = PasswordField('输入密码', validators=[
+        Required(), EqualTo('password2', message='两次输入密码不一致')])
+    password2 = PasswordField('确认密码', validators=[Required()])
+    submit_reg = SubmitField('注册')
+
