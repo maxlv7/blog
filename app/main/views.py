f29a8b9fe25ee4e9a3f8ed46d293109100c4c7b9
maxlv7 <admin@maxlv.org>
2018-03-13 10:44:28 +0800

    admin views

--- app/main/views.py
+++ app/main/views.py
@@ -2,29 +2,43 @@ from flask import render_template,url_for,redirect
 from ..models import Blog
 from . import main
 
+@main.route('/',methods=["GET"])
 @main.route('/index',methods=['GET'])
 def index():
-    return render_template("index.html")
+
+    length = len(Blog.query.all())
+    new_blogs = Blog.query.order_by(Blog.id)
+    l = list()
+    l.append(new_blogs[length-1])
+    l.append(new_blogs[length-2])
+
+    # return "ok"
+    return render_template("index.html",blogs=l)
 
 @main.route('/about',methods=['GET'])
 def about():
+
     return render_template("about.html")
 
 @main.route('/contact',methods=['GET'])
 def contact():
+
     return render_template("contact.html")
 
-@main.route('/content',methods=['GET'])
-def content():
-    return render_template("content.html")
+@main.route('/content/<int:id>',methods=['GET'])
+def blog_content(id):
+
+    blog = Blog.query.filter_by(id=id).first_or_404()
+    return render_template("content.html",blog=blog)
 
 @main.route('/links',methods=['GET'])
 def links():
+
     return render_template("links.html")
 
 @main.route('/blogs',methods=['GET'])
 def blogs():
 
-    blogs = Blog.query.filter_by(id=1).all()
+    blogs = Blog.query.all()
 
     return render_template("blogs.html",blogs=blogs)
